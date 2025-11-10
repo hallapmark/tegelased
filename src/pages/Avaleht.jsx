f2eb96fa51f548ab67bf7abb61f4795a5d90804a
Mark Hallap <hallapmark@gmail.com>
2025-11-10 12:41:58 +0200

    Dynamically display list of elements

--- src/pages/Avaleht.jsx
+++ src/pages/Avaleht.jsx
@@ -1,31 +1,31 @@
+import { useState } from "react";
+
 function Avaleht() {
+  const tegelased = [
+    { eesnimi: "Mickey", perenimi: "Mouse", elukoht: "Disneyland" },
+    { eesnimi: "Minnie", perenimi: "Mouse", elukoht: "Disneyland" },
+    { eesnimi: "Winnie", perenimi: "Pooh", elukoht: "Hundred Acre Wood" },
+    { eesnimi: "Roo", perenimi: "Kangaroo", elukoht: "Hundred Acre Wood" },
+    { eesnimi: "Scooby", perenimi: "Doo", elukoht: "Crystal Cove" },
+  ]
+  const [klikitudNimi, uuendaKlikitudNimi] = useState("");
+
+  const kuvaNimi = (tegelane) => {
+    // console.log(tegelane.eesnimi);
+    uuendaKlikitudNimi(tegelane.eesnimi)
+  }
+
   return (
     <div>
-      <div>
-        <div>Mickey</div>
-        <div>Mouse</div>
-        <div>Disneyland</div>
-      </div>
-      <div>
-        <div>Minnie</div>
-        <div>Mouse</div>
-        <div>Disneyland</div>
-      </div>
-      <div>
-        <div>Winnie</div>
-        <div>Pooh</div>
-        <div>Hundred Acre Wood</div>
-      </div>
-      <div>
-        <div>Roo</div>
-        <div>Kangaroo</div>
-        <div>Hundred Acre Wood</div>
-      </div>
-      <div>
-        <div>Scooby</div>
-        <div>Doo</div>
-        <div>Crystal Cove</div>
-      </div>
+      { klikitudNimi !== "" && <div>Klikkisid tegelase {klikitudNimi} peal</div> }
+      {tegelased.map(tegelane =>
+        <div key={tegelane.eesnimi} style={{display: "flex", gap: "18px", justifyContent: "space-between", margin: "10px"}}>
+          <div>{tegelane.eesnimi}</div>
+          <div>{tegelane.perenimi}</div>
+          <div>{tegelane.elukoht}</div>
+          <button onClick={() => kuvaNimi(tegelane)}>Kuva nimi</button>
+        </div>
+      )}
     </div>
   );
 }
